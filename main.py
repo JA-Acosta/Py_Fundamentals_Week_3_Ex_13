3dc20407478578ed955682fa65522e899da0c3df
JAAR <joe.aaron.acosta@outlook.com>
2023-09-19 18:28:09 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 09/19/2023
+>>> Practicing Fundamentals Program 25
+>>> Version 0
+'''
+
+'''
+>>> Generates a program that takes inputs from the user and does computational analysis then returns the answer.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
